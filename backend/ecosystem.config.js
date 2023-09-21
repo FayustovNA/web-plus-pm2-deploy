17e0c3b8684aecef27de73a1bd3080cfea8102ff
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-09-21 21:14:19 +0300

    deploy backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,12 +1,16 @@
 require('dotenv').config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
   apps: [{
-    name: 'api-service',
+    name: 'mesto',
     script: './dist/app.js',
   }],
 
@@ -15,10 +19,11 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/FayustovNA/web-plus-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
-};
+
+}
