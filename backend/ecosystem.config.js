3d69fb34c155a94f7aa48e6472390abf50373aee
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-09-24 06:56:36 +0300

    240902

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js -- env production`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js  --env production`,
     },
   },
 
